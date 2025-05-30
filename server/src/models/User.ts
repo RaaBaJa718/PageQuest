24cc94d5e08d43399ddca4873334561f628b7947
Ruben <ruben.alston646@gmail.com>
2025-05-30 00:55:40 -0400

    fix: update import statement for bookSchema in User model

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,8 +1,6 @@
 import { Schema, model, type Document } from 'mongoose';
 import bcrypt from 'bcrypt';
-
-// import schema from Book.js
-import bookSchema from './Book.js';
+import { bookSchema } from './Book.js';
 import type { BookDocument } from './Book.js';
 
 export interface UserDocument extends Document {
